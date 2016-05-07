385987d6ae22a1f2ab6161fe2332f208007eadde
eab03 <elizabethbright@Elizabeths-MacBook-Air.local>
2016-05-07 13:54:47 -0400

    Experimented with hearts

--- js/custom.js
+++ js/custom.js
@@ -7,38 +7,6 @@ var counter = 0;
 var leftheart = document.getElementById("leftheartimage");
 var rightheart = document.getElementById("rightheartimage");
 
-// var heartListLeft = [
-//     "./images/left/heartleft1.png",
-//     "./images/left/heartleft2.png",
-//     "./images/left/heartleft3.png",
-//     "./images/left/heartleft4.png",
-//     "./images/left/heartleft5.png",
-//     "./images/left/heartleft6.png",
-//     "./images/left/heartleft7.png",
-//     "./images/left/heartleft8.png",
-//     "./images/left/heartleft9.png",
-//     "./images/left/heartleft10.png",
-//     "./images/left/heartleft11.png",
-//     "./images/left/heartleft12.png",
-//     "./images/left/heartleft13.png"
-// ];
-
-// var heartListRight = [
-//     "./images/right/heartright1.png",
-//     "./images/right/heartright2.png",
-//     "./images/right/heartright3.png",
-//     "./images/right/heartright4.png",
-//     "./images/right/heartright5.png",
-//     "./images/right/heartright6.png",
-//     "./images/right/heartright7.png",
-//     "./images/right/heartright8.png",
-//     "./images/right/heartright9.png",
-//     "./images/right/heartright10.png",
-//     "./images/right/heartright11.png",
-//     "./images/right/heartright12.png",
-//     "./images/right/heartright13.png"
-// ];
-
 var button1 = document.getElementById("bb-nav-first");
 var button2 = document.getElementById("bb-nav-prev");
 var button3 = document.getElementById("bb-nav-next");
@@ -48,16 +16,18 @@ var button4 = document.getElementById("bb-nav-last");
 
 //first button
 
-button1.addEventListener("click", function() {
+var clickFirst = function() {
     counter = 0;
     leftheart.src = "./images/left/heartleft3.png";
     rightheart.src = "./images/right/heartright3.png";
     console.log(counter);
-});
+};
+
+button1.addEventListener("click", clickFirst);
 
 //button previous
 
-button2.addEventListener("click", function() {
+var updatePage = function() {
     // counter = counter - 1;
     if (counter > 0) {
         counter = counter - 1;
@@ -126,75 +96,13 @@ button2.addEventListener("click", function() {
         counter = counter;
     }
     console.log(counter);
-});
+};
+
+button2.addEventListener("click", updatePage);
 
 //button next
 
-button3.addEventListener('click', function() {
-    // counter = counter + 1;
-    if (counter + 1 <= 50) {
-        counter = counter + 1;
-    }
-    if (1 <= counter && counter <= 4) {
-        leftheart.src = "./images/left/heartleft4.png";
-        rightheart.src = "./images/right/heartright4.png";
-    }
-    if (5 <= counter && counter <= 5) {
-        leftheart.src = "./images/left/heartleft3.png";
-        rightheart.src = "./images/right/heartright3.png";
-    }
-    if (6 <= counter && counter <= 10) {
-        leftheart.src = "./images/left/heartleft5.png";
-        rightheart.src = "./images/right/heartright5.png";
-    }
-    if (11 <= counter && counter <= 16) {
-        leftheart.src = "./images/left/heartleft9.png";
-        rightheart.src = "./images/right/heartright9.png";
-    }
-    if (17 <= counter && counter <= 21) {
-        leftheart.src = "./images/left/heartleft10.png";
-        rightheart.src = "./images/right/heartright10.png";
-    }
-    if (22 <= counter && counter <= 22) {
-        leftheart.src = "./images/left/heartleft12.png";
-        rightheart.src = "./images/right/heartright12.png";
-    }
-    if (23 <= counter && counter <= 24) {
-        leftheart.src = "./images/left/heartleft2.png";
-        rightheart.src = "./images/right/heartright2.png";
-    }
-    if (25 <= counter && counter <= 25) {
-        leftheart.src = "./images/left/heartleft6.png";
-        rightheart.src = "./images/right/heartright6.png";
-    }
-    if (26 <= counter && counter <= 30) {
-        leftheart.src = "./images/left/heartleft13.png";
-        rightheart.src = "./images/right/heartright13.png";
-    }
-    if (31 <= counter && counter <= 31) {
-        leftheart.src = "./images/left/heartleft11.png";
-        rightheart.src = "./images/right/heartright11.png";
-    }
-    if (32 <= counter && counter <= 34) {
-        leftheart.src = "./images/left/heartleft7.png";
-        rightheart.src = "./images/right/heartright7.png";
-    }
-    if (35 <= counter && counter <= 35) {
-        leftheart.src = "./images/left/heartleft11.png";
-        rightheart.src = "./images/right/heartright11.png";
-    }
-    if (36 <= counter && counter <= 44) {
-        leftheart.src = "./images/left/heartleft2.png";
-        rightheart.src = "./images/right/heartright2.png";
-    }
-    if (45 <= counter && counter <= 49) {
-        leftheart.src = "./images/left/heartleft1.png";
-        rightheart.src = "./images/right/heartright1.png";
-    } else {
-        counter = counter;
-    }
-    console.log(counter);
-});
+button3.addEventListener('click', updatePage);
 
 //button last
 
